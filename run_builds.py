bc39914b55199d2fcf4af6c8313f09526cb4f5c7
Mike Giroux <rmgiroux@gmail.com>
2016-01-07 12:55:28 -0500

    Adjust formatting

--- run_builds.py
+++ run_builds.py
@@ -12,12 +12,14 @@ import sys
 import re
 
 from pylibinit import addlibpath
+
 addlibpath.add_lib_path()
 
 from blessings import Terminal
 from tqdm import format_meter
 
 term = Terminal()
+
 
 class OutputCollector:
     def __init__(self, name):
@@ -27,7 +29,7 @@ class OutputCollector:
     def process_line(self, stream):
         while not stream.at_eof():
             line = yield from stream.readline()
-            print("%s: %s"%(name, line))
+            print("%s: %s" % (self.name, line))
 
 
 @asyncio.coroutine
@@ -40,10 +42,12 @@ def read_stdout(stream, callback):
         else:
             callback(line)
 
+
 @asyncio.coroutine
 def async_exec(command, stdoutCallback):
-    fork = yield from asyncio.create_subprocess_shell(
-        (command),stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    fork = yield from asyncio.create_subprocess_shell((command),
+                                                      stdout=subprocess.PIPE,
+                                                      stderr=subprocess.STDOUT)
 
     tasks = []
     if fork.stdout is not None:
@@ -57,62 +61,63 @@ def async_exec(command, stdoutCallback):
 
     return retCode
 
-def run_waf(directory, ufid, position, target=None):
-    prefix="%s/_build/%s"%(directory, ufid)
-    waflock=".waf-lock-%s"%ufid
-    build_dir="_build/%s"%(ufid)
 
-    environment="export PREFIX=%s WAFLOCK=%s BDE_WAF_UFID=%s BDE_WAF_BUILD_DIR=%s"%(prefix,waflock,ufid,build_dir)
+def run_waf(directory, ufid, position, target=None):
+    prefix = "%s/_build/%s" % (directory, ufid)
+    waflock = ".waf-lock-%s" % ufid
+    build_dir = "_build/%s" % (ufid)
+
+    environment = (
+        "export PREFIX=%s WAFLOCK=%s BDE_WAF_UFID=%s BDE_WAF_BUILD_DIR=%s" % (
+            prefix, waflock, ufid, build_dir))
 
     waf_command = "waf configure clean build --test=run -j6 -k"
 
     if target is not None:
-        waf_command += (" --target=%s"%target)
+        waf_command += (" --target=%s" % target)
 
-    command="%s; cd %s; %s 2>&1 | tee %s.out "%(environment, directory, waf_command, ufid)
+    command = "%s; cd %s; %s 2>&1 | tee %s.out " % (
+        environment, directory, waf_command, ufid)
 
-    #with term.location(1, 30 + (position * 5)):
+    # with term.location(1, 30 + (position * 5)):
     #    print("Starting command %s"%command)
 
-    task=async_exec(command,
-                    lambda x: line_callback(position, ufid, x))
+    task = async_exec(command, lambda x: line_callback(position, ufid, x))
 
     return task
 
 
-regex=b"\[\s*(\d+)/\s*(\d+)\s*\] \w+\s+(.*)"
-progress_regex=re.compile(regex)
+regex = b"\[\s*(\d+)/\s*(\d+)\s*\] \w+\s+(.*)"
+progress_regex = re.compile(regex)
 
-test_summary=b"Test Summary"
-test_summary_regex=re.compile(test_summary)
+test_summary = b"Test Summary"
+test_summary_regex = re.compile(test_summary)
 
-test_pass=b"All tests passed."
-test_pass_regex=re.compile(test_pass)
+test_pass = b"All tests passed."
+test_pass_regex = re.compile(test_pass)
 
-test_fail=b"tests have fail"
-test_fail_regex=re.compile(test_fail)
+test_fail = b"tests have fail"
+test_fail_regex = re.compile(test_fail)
+
 
 def line_callback(position, ufid, line):
     line = line.rstrip()
 
     if debug_mode:
-        with term.location(1, 30 + position*2):
+        with term.location(1, 30 + position * 2):
             print(" " * 80)
-        with term.location(1, 30 + position*2):
+        with term.location(1, 30 + position * 2):
             print("%-20s: %-50s" % (ufid, line.decode("ascii")[-50:]))
 
     match = progress_regex.search(line)
     if match is not None:
         with term.location(1, position * 2):
-            print(format_meter(int(match.group(1)),
-                               int(match.group(2)),
-                               0,
-                               ascii=True,
-                               prefix = ("%-20s"%ufid)))
+            print(format_meter(int(match.group(1)), int(match.group(2)), 0,
+                               ascii=True, prefix=("%-20s" % ufid)))
         with term.location(5, position * 2 + 1):
             print("%60s" % match.group(3).decode("ascii")[-60:])
 
-        return                                                         # RETURN
+        return  # RETURN
 
     match = test_summary_regex.search(line)
     if match is not None:
@@ -122,15 +127,16 @@ def line_callback(position, ufid, line):
             print("%-60s" % line.decode("ascii")[-60:])
 
         if debug_mode:
-            with term.location(1, 40 + position*3):
-                print("%-20s: Test summary matched"%ufid)
+            with term.location(1, 40 + position * 3):
+                print("%-20s: Test summary matched" % ufid)
 
             with term.location(1, 75):
-                print("%-20s: test summary match - hit enter to continue"%ufid)
+                print(
+                        "%-20s: test summary match - hit enter to continue" % ufid)
 
                 my_input = sys.stdin.readline()
 
-        return                                                         # RETURN
+        return  # RETURN
 
     match = test_pass_regex.search(line)
     if match is not None:
@@ -138,16 +144,16 @@ def line_callback(position, ufid, line):
             print("[%-60s]" % term.green(line.decode("ascii")[-60:]))
 
         if debug_mode:
-            with term.location(1, 41 + position*3):
-                print("%-20s: Test pass regex matched"%ufid)
+            with term.location(1, 41 + position * 3):
+                print("%-20s: Test pass regex matched" % ufid)
 
             with term.location(1, 75):
-                print("%-20s: test pass    match - hit enter to continue"%ufid)
+                print(
+                        "%-20s: test pass    match - hit enter to continue" % ufid)
 
                 my_input = sys.stdin.readline()
 
-        return                                                         # RETURN
-
+        return  # RETURN
 
     match = test_fail_regex.search(line)
     if match is not None:
@@ -155,15 +161,16 @@ def line_callback(position, ufid, line):
             print("[%-60s]" % term.red(line.decode("ascii")[-60:]))
 
         if debug_mode:
-            with term.location(1, 42 + position*3):
-                print("%-20s: Test fail regex matched"%ufid)
+            with term.location(1, 42 + position * 3):
+                print("%-20s: Test fail regex matched" % ufid)
 
             with term.location(1, 75):
-                print("%-20s: test fail    match - hit enter to continue"%ufid)
+                print(
+                        "%-20s: test fail    match - hit enter to continue" % ufid)
 
                 my_input = sys.stdin.readline()
 
-        return                                                         # RETURN
+        return  # RETURN
 
 
 loop = asyncio.get_event_loop()
@@ -174,9 +181,11 @@ tasks = []
 position = 1
 tasks.append(run_waf(checkout_path, "opt_exc_mt", position, target="bsls"))
 position += 1
-tasks.append(run_waf(checkout_path, "dbg_exc_mt_64", position, target="bslscm"))
+tasks.append(
+        run_waf(checkout_path, "dbg_exc_mt_64", position, target="bslscm"))
 position += 1
-tasks.append(run_waf(checkout_path, "dbg_exc_mt_cpp11", position, target="bsls"))
+tasks.append(
+        run_waf(checkout_path, "dbg_exc_mt_cpp11", position, target="bsls"))
 position += 1
 
 with term.location(1, (position + 2) * 2):
